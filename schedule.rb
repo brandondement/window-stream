e060535d7df4bf9631693a123a3e7326fdc9d47b
brandondement <brandon.dement@gmail.com>
2017-05-11 15:54:42 -0700

    simple scheduling w/ at

--- schedule.rb
+++ schedule.rb
@@ -1,19 +1,20 @@
 require 'open_weather'
+# require 'pp'
+
+SECS_IN_HOUR = 60 * 60;
 
 resp = OpenWeather::Current.city(
   "San Francisco, CA",
   { APPID: 'fc56307bd6990a44569e402e2146a03b' }
 )
-
-# require 'pp'
 # pp resp
 
-sunrise = Time.at(resp['sys']['sunrise'])
-sunset = Time.at(resp['sys']['sunset'])
-puts "sunrise: #{sunrise}"
-puts "sunset: #{sunset}"
+cmd = "ruby #{Dir.pwd}/record.rb"
 
-# record at 30 mins before sunrise
-puts sunrise.to_i
+# start recording 30 mins before sunrise
+sunrise_start = Time.at(resp['sys']['sunrise']) - (SECS_IN_HOUR / 2)
+`echo '#{cmd}' | at #{sunrise_start.strftime "%H:%M"}`
 
-# record at 1 hr before sunset
+# start recording 1 hr before sunset
+sunset_start = Time.at(resp['sys']['sunset']) - SECS_IN_HOUR
+`echo '#{cmd}' | at #{sunset_start.strftime "%H:%M"}`

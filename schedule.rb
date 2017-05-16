46f1c6154c48f3dac9945be1af0eb2e24726cfac
brandondement <brandondement@users.noreply.github.com>
2017-05-15 22:23:55 -0700

    using system() rather than backtick

--- schedule.rb
+++ schedule.rb
@@ -9,30 +9,10 @@ resp = OpenWeather::Current.city(
 )
 # pp resp
 
-# connect to gopro
-`sudo #{Dir.pwd}/connect-to-gopro.sh`
-sleep 2
-
-# record
-duration = SECS_IN_HOUR * 1.5
-`ruby #{Dir.pwd}/record.rb #{duration}`
-# sleep duration
-
-# connect to home network
-`sudo #{Dir.pwd}/connect-to-home.sh`
-sleep 2
-
-# upload
-`ruby #{Dir.pwd}/upload.rb`
-puts cmd
-
-
-exit
-
 # start recording 30 mins before sunrise
 sunrise_start = Time.at(resp['sys']['sunrise']) - (SECS_IN_HOUR / 2)
-`echo '#{cmd}' | at #{sunrise_start.strftime "%H:%M"}`
+system("echo '#{Dir.pwd}/ruby task.rb' | at #{sunrise_start.strftime "%H:%M"}")
 
 # start recording 1 hr before sunset
 sunset_start = Time.at(resp['sys']['sunset']) - SECS_IN_HOUR
-`echo '#{cmd}' | at #{sunset_start.strftime "%H:%M"}`
+system("echo '#{Dir.pwd}/ruby task.rb' | at #{sunset_start.strftime "%H:%M"}")

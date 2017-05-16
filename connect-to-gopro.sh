b336c667d459d8f613a88ef4008201c1f4354f3d
brandondement <brandondement@users.noreply.github.com>
2017-05-16 09:22:13 -0700

    tweaked wait times

--- connect-to-gopro.sh
+++ connect-to-gopro.sh
@@ -1,4 +1,4 @@
 cp /etc/wpa_supplicant/wpa_supplicant.gopro.conf /etc/wpa_supplicant/wpa_supplicant.conf;
 wpa_cli reconfigure;
-sleep 2;
+sleep 5;
 iwconfig;
--- connect-to-home.sh
+++ connect-to-home.sh
@@ -1,4 +1,4 @@
 cp /etc/wpa_supplicant/wpa_supplicant.home.conf /etc/wpa_supplicant/wpa_supplicant.conf;
 wpa_cli reconfigure;
-sleep 2;
+sleep 3;
 iwconfig;

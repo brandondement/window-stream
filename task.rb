890089eec11dc556b1177aeb06b69127b43d472b
brandondement <brandondement@users.noreply.github.com>
2017-05-18 14:03:49 -0700

    tweaked timing, added cleanup

--- task.rb
+++ task.rb
@@ -4,19 +4,25 @@ SECS_IN_HOUR = 60 * 60;
 
 # connect to gopro
 system("sudo #{Dir.pwd}/connect-to-gopro.sh")
-sleep 2
+
+# wait to establish connection
+sleep 8
 
 # record
 duration = 5 # SECS_IN_HOUR * 1.5
 system("ruby #{Dir.pwd}/record.rb #{duration}")
-# sleep duration
 
 # download
 system("ruby #{Dir.pwd}/download.rb")
 
 # connect to home network
 system("sudo #{Dir.pwd}/connect-to-home.sh")
-sleep 2
+
+# wait to establish connection
+sleep 8
 
 # upload
 system("ruby #{Dir.pwd}/upload.rb")
+
+# cleanup
+system("rm #{Dir.pwd}/*.MP4")

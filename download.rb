c99ee3e3cdd3d741d9f1d0cec7811ccb7b834cd4
brandondement <brandondement@users.noreply.github.com>
2017-05-31 10:32:41 -0700

    streaming file

--- download.rb
+++ download.rb
@@ -1,5 +1,4 @@
 require 'goprocam'
-require 'aws-sdk'
 require 'open-uri'
 
 # cant connect to camera
@@ -8,8 +7,22 @@ gpCamera = Camera.new
 # puts gpCamera.overview()
 sleep 1
 # puts gpCamera.list_media() # json
-file = gpCamera.get_media()
-puts "downloading: " + file
-gpCamera.dl_media() # open-uri: file.puts f.read
-puts "downloaded: " + file
-puts "You shoud get internet now to upload..."
+
+large_file = gpCamera.get_media()
+puts "downloading: " + large_file
+
+uri = URI(large_file)
+
+Net::HTTP.start(uri.host, uri.port) do |http|
+  request = Net::HTTP::Get.new uri
+  http.request request do |response|
+    open "video.MP4", 'w' do |io|
+      response.read_body do |chunk|
+        io.write chunk
+      end
+    end
+  end
+end
+
+puts "downloaded: " + large_file
+puts "You should get internet now to upload..."

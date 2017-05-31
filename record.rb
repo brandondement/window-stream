ec94ccdde63cea98e9c84c1b640eb007f025c3e3
brandondement <brandondement@users.noreply.github.com>
2017-05-31 16:42:36 -0700

    added default recording options

--- record.rb
+++ record.rb
@@ -7,8 +7,22 @@ end
 
 gpCamera = Camera.new
 sleep 1
+
+gpCamera.sync_time()
+sleep 1
+
+gpCamera.camera_mode(Mode::VideoMode, Mode::SubMode::Video::TimeLapseVideo)
+sleep 1
+# gpCamera.gpControlCommand(Multishot::TIMELAPSE_INTERVAL, Multishot::TimeLapseInterval::I10)
+# sleep 1
+gpCamera.gpControlCommand(Video::RESOLUTION, Video::Resolution::R1080p)
+sleep 1
+gpCamera.gpControlCommand(Video::FOV, Video::Fov::Narrow)
+sleep 1
+
 gpCamera.shutter(Shutter::ON)
 sleep ARGV[0].to_i
 gpCamera.shutter(Shutter::OFF)
 sleep 1
+
 puts 'recorded ' + gpCamera.get_media()
